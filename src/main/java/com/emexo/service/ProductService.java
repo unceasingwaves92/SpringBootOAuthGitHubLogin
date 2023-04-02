300fa7772c8a2482324b9e57f3f664b29fa8d1ed
karthikeyan <unceasingwaves92@gmail.com>
2023-04-02 20:26:29 +0530

    initial commit

---
+++ src/main/java/com/emexo/service/ProductService.java
@@ -0,0 +1,30 @@
+package com.emexo.service;
+
+import java.util.List;
+
+import com.emexo.entity.Product;
+import com.emexo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll() {
+		return repo.findAll();
+	}
+	
+	public void save(Product product) {
+		repo.save(product);
+	}
+	
+	public Product get(Long id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+}
